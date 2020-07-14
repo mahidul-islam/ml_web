8e9ff5244201e86b75bf9c01c92689d32cedeb44
mahidul-islam <mizihan84@gmail.com>
2020-07-14 12:26:46 +0600

    added celery broker url

--- ml_web/users/models.py
+++ ml_web/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -10,6 +11,7 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    test = models.BooleanField(default=False)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
